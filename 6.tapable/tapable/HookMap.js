1ef9aec12627110a4e510c5505da481a55a5dc6a
pengfei.zuo <pengfei.zuo@retech.work>
2023-03-27 18:26:13 +0800

    feat(4.tapable) hookmap注释

--- 6.tapable/tapable/HookMap.js
+++ 6.tapable/tapable/HookMap.js
@@ -12,9 +12,14 @@ class HookMap {
   tapPromise(key, options, fn) {
     return this.for(key).tapPromise(options, fn);
   }
+  // 按key对钩子进行分组，
+  // 使用传入的钩子工厂创建hook
   for(key) {
+    // key对应的钩子存在就返回
     const hook = this.get(key);
     if (hook) return hook;
+
+    // 使用工厂创建新的钩子并返回
     let newHook = this._factory();
     this._map.set(key, newHook);
     return newHook;
