4ea2af76442a211d0204dba7d1f0fef40cbe0ad7
pengfei.zuo <pengfei.zuo@retech.work>
2023-03-28 19:35:10 +0800

    feat(7.plugins) dist目录下所有的文件打在一个压缩包插件会打两个zip文件的原因

--- 7.plugins/webpack.config.js
+++ 7.plugins/webpack.config.js
@@ -3,14 +3,15 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const DonePlugin = require("./plugins/done-plugin");
 const ArchivePlugin = require("./plugins/archive-plugin");
 
+// 加了HtmlWebpackPlugin这个插件后ArchivePlugin会走两次
 module.exports = {
   mode: "development",
   devtool: false,
   entry: "./src/index.js",
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
+    // new HtmlWebpackPlugin({
+    //   template: "./src/index.html",
+    // }),
     // new DonePlugin(),
     new ArchivePlugin({ filename: "[timestamp].zip" }),
   ],
